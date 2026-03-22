be04e15a0423f952951c36ead1ebf8aff5197fde
wangjingjun <mm_1388qq.com>
2026-03-22 18:49:17 +0800

    link plugin

--- web/src/view/promotion/template-widget/index.vue
+++ web/src/view/promotion/template-widget/index.vue
@@ -11,12 +11,6 @@
             <el-option label="电脑端二维码插件" :value="5" />
           </el-select>
         </el-form-item>
-        <el-form-item label="状态">
-          <el-select v-model="search.status" clearable placeholder="选择状态" style="width: 120px">
-            <el-option label="启用" :value="1" />
-            <el-option label="禁用" :value="0" />
-          </el-select>
-        </el-form-item>
         <el-form-item>
           <el-button type="primary" icon="search" @click="onSubmit">查询</el-button>
           <el-button icon="refresh" @click="onReset">重置</el-button>
@@ -33,13 +27,6 @@
         <el-table-column label="类型" width="160">
           <template #default="{ row }">
             {{ typeMap[row.type] }}
-          </template>
-        </el-table-column>
-        <el-table-column label="状态" width="100">
-          <template #default="{ row }">
-            <el-tag :type="row.status === 1 ? 'success' : 'danger'">
-              {{ row.status === 1 ? '启用' : '禁用' }}
-            </el-tag>
           </template>
         </el-table-column>
         <el-table-column prop="createdAt" label="创建时间" width="180" />
@@ -62,26 +49,20 @@
       />
     </div>
     <el-dialog v-model="drawer" title="模板插件编辑" width="800px" :close-on-click-modal="false">
-      <el-form ref="formRef" :model="form" label-width="100px">
-        <el-form-item label="名称" prop="name" :rules="[{ required: true, message: '请输入名称', trigger: 'blur' }]">
-          <el-input v-model="form.name" placeholder="请输入名称" />
-        </el-form-item>
-        <el-form-item label="类型" prop="type" :rules="[{ required: true, message: '请选择类型', trigger: 'change' }]">
-          <el-select v-model="form.type" placeholder="请选择类型">
-            <el-option label="手机端模板" :value="1" />
-            <el-option label="电脑端模板" :value="2" />
-            <el-option label="手机复制插件" :value="3" />
-            <el-option label="手机底部插件" :value="4" />
-            <el-option label="电脑端二维码插件" :value="5" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
-          <el-switch v-model="form.status" active-text="启用" inactive-text="禁用" />
-        </el-form-item>
-        <el-form-item label="内容" prop="content" :rules="[{ required: true, message: '请输入内容', trigger: 'blur' }]">
-          <RichEdit v-model="form.content" :height="400" />
-        </el-form-item>
-      </el-form>
+       <el-form ref="formRef" :model="form" label-width="100px">
+          <el-form-item label="名称" prop="name" :rules="[{ required: true, message: '请输入名称', trigger: 'blur' }]">
+            <el-input v-model="form.name" placeholder="请输入名称" />
+          </el-form-item>
+          <el-form-item label="类型" prop="type" :rules="[{ required: true, message: '请选择类型', trigger: 'change' }]">
+            <el-select v-model="form.type" placeholder="请选择类型">
+              <el-option label="手机端模板" :value="1" />
+              <el-option label="电脑端模板" :value="2" />
+              <el-option label="手机复制插件" :value="3" />
+              <el-option label="手机底部插件" :value="4" />
+              <el-option label="电脑端二维码插件" :value="5" />
+            </el-select>
+          </el-form-item>
+        </el-form>
       <template #footer>
         <el-button @click="drawer = false">取消</el-button>
         <el-button type="primary" @click="submit">确定</el-button>
@@ -93,7 +74,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
-import RichEdit from '@/components/richtext/rich-edit.vue'
 import {
   getTemplateWidgetList, createTemplateWidget, updateTemplateWidget, deleteTemplateWidget
 } from '@/api/promotion'
@@ -106,7 +86,7 @@ const typeMap = {
   5: '电脑端二维码插件'
 }
 
-const search = ref({ type: null, status: null })
+const search = ref({ type: null })
 const tableData = ref([])
 const page = ref(1)
 const pageSize = ref(10)
@@ -116,9 +96,7 @@ const formRef = ref()
 const form = ref({
   ID: 0,
   name: '',
-  type: null,
-  content: '',
-  status: 1
+  type: null
 })
 
 const getTableData = async () => {
@@ -142,7 +120,7 @@ const onSubmit = () => {
 }
 
 const onReset = () => {
-  search.value = { type: null, status: null }
+  search.value = { type: null }
   onSubmit()
 }
 
@@ -160,9 +138,7 @@ const openForm = (row) => {
   form.value = row ? { ...row } : {
     ID: 0,
     name: '',
-    type: null,
-    content: '',
-    status: 1
+    type: null
   }
   drawer.value = true
 }
