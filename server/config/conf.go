86e16e7271589e4afe2a344ef0d29398280baa9e
wangjingjun <mm_1388qq.com>
2026-06-08 21:58:15 +0800

    fix 域名

--- server/config/conf.go
+++ server/config/conf.go
@@ -2,4 +2,5 @@ package config
 
 type Conf struct {
 	MainDomain string `mapstructure:"main-domain" json:"main-domain" yaml:"main-domain"`
+	LdyDomain  string `mapstructure:"ldy-domain" json:"ldy-domain" yaml:"ldy-domain"`
 }
