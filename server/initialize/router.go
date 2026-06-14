b29fc178e6ab44dc05142650e33fce7436295715
wangjingjun <mm_1388qq.com>
2026-06-14 09:17:33 +0800

    fix 跨域请求

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -121,8 +121,8 @@ func Routers() *gin.Engine {
 	})
 	// 跨域，如需跨域可以打开下面的注释
 	// Router.Use(middleware.Cors()) // 直接放行全部跨域请求
-	// Router.Use(middleware.CorsByRules()) // 按照配置的规则放行跨域请求
-	// global.GVA_LOG.Info("use middleware cors")
+	Router.Use(middleware.CorsByRules()) // 按照配置的规则放行跨域请求
+	global.GVA_LOG.Info("use middleware cors")
 	docs.SwaggerInfo.BasePath = global.GVA_CONFIG.System.RouterPrefix
 	Router.GET(global.GVA_CONFIG.System.RouterPrefix+"/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	global.GVA_LOG.Info("register swagger handler")
